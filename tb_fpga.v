95077fdebbee427e70d18731d90ad0838e08d32f
Will Long <will.long043@gmail.com>
2016-06-03 23:15:52 -0400

    Made a very basic test work on the testbench.

--- tb_fpga.v
+++ tb_fpga.v
@@ -38,11 +38,11 @@ module tb_fpga();
 		topioselect = 30'b0;
 		bottomioselect = 30'b0;
 		$display("initialized memory");
-		// // set_bottom_io_cfg(0, 0, 1); // Bottom left in
-		bottomioselect[1] = 1'b1;
+		#10 set_bottom_io_cfg(0, 0, 1); // Bottom left in
+		// // bottomioselect[1] = 1'b1;
 		// set_top_io_cfg(0, 0, 1); // Top left out
-		// // set_left_io_cfg(0, 0, 2);
-		leftioselect[0] = 1'b1;
+		#10 set_left_io_cfg(0, 0, 2);
+		// // leftioselect[0] = 1'b1;
 		// // set_brb_cfg(0, 0, 0, 2, 2);
 		brbselect[4] = 1'b0;
 		brbselect[5] = 1'b1;
@@ -51,6 +51,8 @@ module tb_fpga();
 		// set_brb_cfg(2, 0, 0, 3, 1);
 		// set_brb_cfg(3, 0, 0, 3, 1);
 		// set_brb_cfg(4, 0, 0, 3, 1);
+
+		$display("finished setting bits.");
 
 		A = 1'b0; B = 1'b0; carryin = 1'b0;
 		$monitor("A = %b, B = %b, carryin = %b, out = %b, carryout = %b",
@@ -65,15 +67,17 @@ module tb_fpga();
 		#10 A = 1'b1; B = 1'b1; carryin = 1'b1;
 	end
 	task set_top_io_cfg;
-		input io_index, io_line, io_dir;
+		input integer io_index, io_line, io_dir;
 		begin
 			if (io_dir == 0) begin // Off
 				topioselect[io_index*6+io_line*2+0] = 1'b0;
 				topioselect[io_index*6+io_line*2+1] = 1'b0;
 			end else if (io_dir == 1) begin // Out
+				$display("set bit %d in top", io_index*6+io_line*2+1);
 				topioselect[io_index*6+io_line*2+0] = 1'b0;
 				topioselect[io_index*6+io_line*2+1] = 1'b1;
 			end else if (io_dir == 2) begin // In
+				$display("set bit %d in top", io_index*6+io_line*2+0);
 				topioselect[io_index*6+io_line*2+0] = 1'b1;
 				topioselect[io_index*6+io_line*2+1] = 1'b0;
 			end
@@ -81,15 +85,17 @@ module tb_fpga();
 	endtask
 
 	task set_bottom_io_cfg;
-		input io_index, io_line, io_dir;
+		input integer io_index, io_line, io_dir;
 		begin
 			if (io_dir == 0) begin // Off
 				bottomioselect[io_index*6+io_line*2+0] = 1'b0;
 				bottomioselect[io_index*6+io_line*2+1] = 1'b0;
 			end else if (io_dir == 1) begin // Out
+				$display("set bit %d in bottom", io_index*6+io_line*2+1);
 				bottomioselect[io_index*6+io_line*2+0] = 1'b0;
 				bottomioselect[io_index*6+io_line*2+1] = 1'b1;
 			end else if (io_dir == 2) begin // In
+				$display("set bit %d in bottom", io_index*6+io_line*2+0);
 				bottomioselect[io_index*6+io_line*2+0] = 1'b1;
 				bottomioselect[io_index*6+io_line*2+1] = 1'b0;
 			end
@@ -97,15 +103,17 @@ module tb_fpga();
 	endtask
 
 	task set_left_io_cfg;
-		input io_index, io_line, io_dir;
+		input integer io_index, io_line, io_dir;
 		begin
 			if (io_dir == 0) begin // Off
 				leftioselect[io_index*6+io_line*2+0] = 1'b0;
 				leftioselect[io_index*6+io_line*2+1] = 1'b0;
 			end else if (io_dir == 1) begin // Out
+				$display("set bit %d in left", io_index*6+io_line*2+1);
 				leftioselect[io_index*6+io_line*2+0] = 1'b0;
 				leftioselect[io_index*6+io_line*2+1] = 1'b1;
 			end else if (io_dir == 2) begin // In
+				$display("set bit %d in left", io_index*6+io_line*2+0);
 				leftioselect[io_index*6+io_line*2+0] = 1'b1;
 				leftioselect[io_index*6+io_line*2+1] = 1'b0;
 			end
@@ -113,15 +121,17 @@ module tb_fpga();
 	endtask
 
 	task set_right_io_cfg;
-		input io_index, io_line, io_dir;
+		input integer io_index, io_line, io_dir;
 		begin
 			if (io_dir == 0) begin // Off
 				rightioselect[io_index*6+io_line*2+0] = 1'b0;
 				rightioselect[io_index*6+io_line*2+1] = 1'b0;
 			end else if (io_dir == 1) begin // Out
+				$display("set bit %d in right", io_index*6+io_line*2+1);
 				rightioselect[io_index*6+io_line*2+0] = 1'b0;
 				rightioselect[io_index*6+io_line*2+1] = 1'b1;
 			end else if (io_dir == 2) begin // In
+				$display("set bit %d in right", io_index*6+io_line*2+0);
 				rightioselect[io_index*6+io_line*2+0] = 1'b1;
 				rightioselect[io_index*6+io_line*2+1] = 1'b0;
 			end
@@ -129,10 +139,10 @@ module tb_fpga();
 	endtask
 
 	task set_brb_cfg;
-		input row, col;
+		input integer row, col;
 		// which switch element, which internal switch
-		input s_index, s_sel;
-		input dir;
+		input integer s_index, s_sel;
+		input integer dir;
 		begin
 			if (dir == 0) begin // Off
 				brbselect[col*36 + row*180 + s_index*12 + s_sel*2] = 1'b0;
@@ -148,10 +158,10 @@ module tb_fpga();
 	endtask
 
 	task set_bsb_cfg;
-		input row, col;
+		input integer row, col;
 		// which switch element, which internal switch
-		input s_row, s_col, s_sel;
-		input dir;
+		input integer s_row, s_col, s_sel;
+		input integer dir;
 		begin
 			if (dir == 0) begin // Off
 				bsbselect[col*108 + row*432 + s_col*12 + s_row*36 + s_sel*2] = 1'b0;
@@ -167,7 +177,7 @@ module tb_fpga();
 	endtask
 
 	task set_lb_cfg;
-		input row, col;
+		input integer row, col;
 		input [3:0] truth_tbl;
 		input sync;
 		begin
