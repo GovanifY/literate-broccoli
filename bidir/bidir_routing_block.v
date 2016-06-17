36fa0de35e692b58f131cc632efa19fa987c8a5f
Will Long <will.long043@gmail.com>
2016-06-17 14:08:30 -0400

    Parameterized the routing and switch blocks.

--- bidir/bidir_routing_block.v
+++ bidir/bidir_routing_block.v
@@ -1,13 +1,16 @@
 module bidir_routing_block(
-	input [35:0] select,
+	input [wire_width*12-1:0] select,
 	// zero, zero in lower left corner
-	inout [2:0] left, 
-	inout [2:0] right,
-	inout [2:0] top,
-	inout [2:0] bottom
+	inout [wire_width-1:0] left, 
+	inout [wire_width-1:0] right,
+	inout [wire_width-1:0] top,
+	inout [wire_width-1:0] bottom
 	);
-	bidir_switch bs1(select[11:0], left[0], right[0], top[0], bottom[0]);
-	bidir_switch bs2(select[23:12], left[1], right[1], top[1], bottom[1]);
-	bidir_switch bs3(select[35:24], left[2], right[2], top[2], bottom[2]);
+	parameter wire_width = 3;
+	genvar i;
+	generate for (i = 0; i < wire_width; i = i + 1)
+	begin
+		bidir_switch bs_inst(select[11+i*12:i*12], left[i], right[i], top[i], bottom[i]);
+	end endgenerate
 endmodule
 
--- bidir/bidir_switch_block.v
+++ bidir/bidir_switch_block.v
@@ -1,49 +1,184 @@
 module bidir_switch_block(
-	input [107:0] select,
-	inout [2:0] left, 
-	inout [2:0] right,
-	inout [2:0] bottom
+	input [wire_width*wire_width*12-1:0] select,
+	inout [wire_width-1:0] left, 
+	inout [wire_width-1:0] right, 
+	inout [wire_width-1:0] bottom
 	);
-	wire nowhere; // surrogate topmost connection
-	// Horizontal wires
-	wire r1c12, r1c23;
-	wire r2c12, r2c23;
-	wire r3c12, r3c23;
+	parameter wire_width = 3;
+	/*wire tb[2:0][2:0];
+	wire lr[2:0][1:0];
 	
 	// First row
-	wire w1, w2, w3;
-	bidir_switch bs1(select[11:0], left[0], r1c12, w1, bottom[0]);
-	bidir_switch bs2(select[23:12], r1c12, r1c23, w2, bottom[1]);
-	bidir_switch bs3(select[35:24], r1c23, right[0], w3, bottom[2]);
+	bidir_switch bs1(
+		.select(select[11:0]), 
+		.left(left[0]), 
+		.right(lr[0][0]), 
+		.top(tb[0][0]), 
+		.bottom(bottom[0])
+	);
+	bidir_switch bs2(
+		.select(select[23:12]), 
+		.left(lr[0][0]), 
+		.right(lr[0][1]), 
+		.top(tb[0][1]), 
+		.bottom(bottom[1])
+	);
+	bidir_switch bs3(
+		.select(select[35:24]), 
+		.left(lr[0][1]),
+		.right(right[0]), 
+		.top(tb[0][2]), 
+		.bottom(bottom[2])
+	);
 	// Second row
-	wire w4, w5, w6;
-	bidir_switch bs4(select[47:36], left[1], r2c12, w4, w1);
-	bidir_switch bs5(select[59:48], r2c12, r2c23, w5, w2);
-	bidir_switch bs6(select[71:60], r2c23, right[1], w6, w3);
-	// Second row
-	wire w7, w8, w9;
-	bidir_switch bs7(select[83:72], left[2], r3c12, w7, w4);
-	bidir_switch bs8(select[95:84], r3c12, r3c23, w8, w5);
-	bidir_switch bs9(select[107:96], r3c23, right[2], w9, w6);
+	bidir_switch bs4(
+		.select(select[47:36]), 
+		.left(left[1]), 
+		.right(lr[1][0]), 
+		.top(tb[1][0]), 
+		.bottom(tb[0][0])
+	);
+	bidir_switch bs5(
+		.select(select[59:48]), 
+		.left(lr[1][0]), 
+		.right(lr[1][1]), 
+		.top(tb[1][1]), 
+		.bottom(tb[0][1])
+	);
+	bidir_switch bs6(
+		.select(select[71:60]), 
+		.left(lr[1][1]), 
+		.right(right[1]), 
+		.top(tb[1][2]), 
+		.bottom(tb[0][2])
+	);
+	// Third row
+	bidir_switch bs7(
+		.select(select[83:72]), 
+		.left(left[2]), 
+		.right(lr[2][0]), 
+		.top(tb[2][0]), 
+		.bottom(tb[1][0])
+	);
+	bidir_switch bs8(
+		.select(select[95:84]), 
+		.left(lr[2][0]), 
+		.right(lr[2][1]), 
+		.top(tb[2][1]), 
+		.bottom(tb[1][0])
+	);
+	bidir_switch bs9(
+		.select(select[107:96]), 
+		.left(lr[2][1]), 
+		.right(right[2]), 
+		.top(tb[2][2]), 
+		.bottom(tb[1][2])
+	);*/
+	wire [wire_width-1:0] top;
 	
-	// First column
-	// Vertical wires
-	/*wire w1, w2;
-	bidir_switch bs1(select[11:0], left[0], r1c12, w1, bottom[0]);
-	bidir_switch bs2(select[23:12], left[1], r2c12, w2, w1);
-	bidir_switch bs3(select[35:24], left[2], r3c12, nowhere, w2);
-	// Second column
-	// Vertical wires
-	wire w3, w4;
-	bidir_switch bs4(select[47:36], r1c12, r1c23, w3, bottom[1]);
-	bidir_switch bs5(select[59:48], r2c12, r2c23, w4, w3);
-	bidir_switch bs6(select[71:60], r3c12, r3c23, nowhere, w4);
-	// Third column
-	// Vertical wires
-	wire w5, w6;
-	bidir_switch bs7(select[83:72], r1c23, right[0], w5, bottom[1]);
-	bidir_switch bs8(select[95:84], r2c23, right[1], w6, w5);
-	bidir_switch bs9(select[107:96], r3c23, right[2], nowhere, w6);*/
+	genvar x, y;
+	wire lr[wire_width:0][wire_width:0];
+	wire tb[wire_width:0][wire_width:0];
+	generate
+	for(y = 0; y < wire_width; y = y + 1) begin
+		for (x = 0; x < wire_width; x = x + 1) begin
+			localparam s_base = (x*12)+(y*wire_width*12);
+			if (x == 0) begin
+				if (y == 0) begin // Bottom left
+					bidir_switch bs(
+						.select(select[s_base+11:s_base]), 
+						.left( left[y] ),
+						.right( lr[y][x+1] ),
+						.top( tb[y+1][x] ),
+						.bottom( bottom[x] )
+					);
+				end
+				else if (y == wire_width-1) begin // Top left
+					bidir_switch bs(
+						.select(select[s_base+11:s_base]), 
+						.left( left[y] ),
+						.right( lr[y][x+1] ),
+						.top( top[x] ),
+						.bottom( tb[y][x] )
+					);
+				end
+				else begin // Some left
+					bidir_switch bs(
+						.select(select[s_base+11:s_base]), 
+						.left( left[y] ),
+						.right( lr[y][x+1] ),
+						.top( tb[y+1][x] ),
+						.bottom( tb[y][x] )
+					);
+				end
+			end
+			else if (x == wire_width-1) begin
+				if (y == 0) begin // Bottom right
+					bidir_switch bs(
+						.select(select[s_base+11:s_base]), 
+						.left( lr[y][x] ),
+						.right( right[y] ),
+						.top( tb[y+1][x] ),
+						.bottom( bottom[x] )
+					);
+				end
+				else if (y == wire_width-1) begin // Top right
+					bidir_switch bs(
+						.select(select[s_base+11:s_base]), 
+						.left( lr[y][x] ),
+						.right( right[y] ),
+						.top( top[x] ),
+						.bottom( tb[y][x] )
+					);
+				end
+				else begin // Some right
+					bidir_switch bs(
+						.select(select[s_base+11:s_base]), 
+						.left( lr[y][x] ),
+						.right( right[y] ),
+						.top( tb[y+1][x] ),
+						.bottom( tb[y][x] )
+					);
+				end
+			end else begin
+				if (y == 0) begin // Bottom edge
+					bidir_switch bs(
+						.select(select[s_base+11:s_base]), 
+						.left( lr[y][x] ),
+						.right( lr[y][x+1] ),
+						.top( tb[y+1][x] ),
+						.bottom( bottom[x] )
+					);
+				end
+				else if (y == wire_width-1) begin // Top edge
+					bidir_switch bs(
+						.select(select[s_base+11:s_base]), 
+						.left( lr[y][x] ),
+						.right( lr[y][x+1] ),
+						.top( top[x] ),
+						.bottom( tb[y][x] )
+					);
+				end
+				else begin // Middle somewhere
+					bidir_switch bs(
+						.select(select[s_base+11:s_base]), 
+						.left( lr[y][x] ),
+						.right( lr[y][x+1] ),
+						.top( tb[y+1][x] ),
+						.bottom( tb[y][x] )
+					);
+				end
+			end
+		
+			/*bidir_switch bs(
+				.left( x==0 ? left[y] : lr[y][x] ),
+				.right( x == wire_width-1 ? right[y] : lr[y][x+1] ),
+				.top( y == wire_width-1 ? top[x] : tb[y+1][x] ),
+				.bottom( y == 0 ? bottom[x] : tb[y][x] )
+			);*/
+		end
+	end
+	endgenerate
 	
 endmodule
 
