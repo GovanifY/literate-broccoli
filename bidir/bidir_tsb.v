b15fb4fcf4f183c029f0051a79dd5f018baa9c26
Will Long <will.long043@gmail.com>
2016-06-27 22:14:43 -0400

    Switched the bidirectional switches to use the primitive bufif1 instead of a written TSB.

--- bidir/bidir_tsb.v
+++ bidir/bidir_tsb.v
@@ -2,7 +2,11 @@ module bidir_tsb(
 	input [1:0] select,
 	inout left, right
 	);
-	tsb t1(select[0], left, right);
-	tsb t2(select[1], right, left);
+	// tsb t1(select[0], left, right);
+	// tsb t2(select[1], right, left);
+
+	// out, in, control
+	bufif1 b1(right, left, select[0]);
+	bufif1 b2(left, right, select[1]);
 endmodule
 
